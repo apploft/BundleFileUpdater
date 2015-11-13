5a96d9a92c85b5c2abc1bce379d9c335c887b324
Michael Kamphausen <michael.kamphausen@apploft.de>
2015-11-13 15:43:46 +0100

    Initial Commit

---
+++ BundleFileUpdater/BundleFileUpdater.h
@@ -0,0 +1,19 @@
+//
+//  BundleFileUpdater.h
+//  BundleFileUpdater
+//
+//  Created by Michael Kamphausen on 13.11.15.
+//  Copyright © 2015 apploft GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BundleFileUpdater.
+FOUNDATION_EXPORT double BundleFileUpdaterVersionNumber;
+
+//! Project version string for BundleFileUpdater.
+FOUNDATION_EXPORT const unsigned char BundleFileUpdaterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BundleFileUpdater/PublicHeader.h>
+
+
